9927c7030ed02cb0408d22283c039685356b19b2
ananaz305 <annnz305@gmail.com>
2025-05-10 19:48:39 +0100

    PostgreSQL on 15432 port

--- reg-login-service/app/core/config.py
+++ reg-login-service/app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     DB_USER: str = os.getenv("DB_USER", "postgres")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "password")
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_PORT: str = os.getenv("DB_PORT", "15432")
     DB_NAME: str = os.getenv("DB_NAME", "reg_login_db")
     DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
