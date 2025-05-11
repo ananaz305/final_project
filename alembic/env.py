1f6a845d374fc32b2cc1b987c51fef564d8d9d1e
ananaz305 <annnz305@gmail.com>
2025-05-12 00:36:05 +0100

    Alembic, DB migration, test users

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,9 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'reg-login-service')))
 from logging.config import fileConfig
 
-from sqlalchemy import create_engine # Используем create_engine для синхронной работы
+from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
-# Импортируем SYNC_DATABASE_URL из нашего конфига
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,23 +14,19 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-from app.db.database import Base
-from app.models import user  # Импортируем модель User
+from app.db.database import Base  # импортируй Base из своего проекта
 
+# внутри run_migrations_online() добавь:
+from app.core.config import settings
 target_metadata = Base.metadata
 
-# Получаем URL для ОФФЛАЙН режима из old_a.ini (если он там есть и нужен)
-# или можно также использовать settings.SYNC_DATABASE_URL
-offline_url = config.get_main_option("sqlalchemy.url")
-if not offline_url: # Если в .ini не указан, берем из settings
-    offline_url = settings.SYNC_DATABASE_URL
+connectable = create_engine(settings.DATABASE_URL)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=offline_url, # Используем URL для оффлайн режима
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -46,14 +37,16 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
-    # Создаем синхронный движок, используя SYNC_DATABASE_URL из settings
-    connectable = create_engine("postgresql+psycopg2://ananaz:ananaz@postgres_db:5432/microservice_db", poolclass=pool.NullPool)
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True  # <-- это позволяет Alembic отследить изменения ENUM
+            target_metadata=target_metadata
         )
 
         with context.begin_transaction():
